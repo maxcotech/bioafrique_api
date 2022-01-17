47e720b8fde7e1c98d84bc39ee8216bac57c3370
Chisom Maxwell <chiscomax2013@gmail.com>
2022-01-17 13:15:01 -0800

    Edited review filters

--- app/Actions/ProductReview/GetReviews.php
+++ app/Actions/ProductReview/GetReviews.php
@@ -21,10 +21,8 @@ class GetReviews extends Action{
       $data = $this->request->all();
       $data['review_id'] = $this->review_id;
       $val = Validator::make($data,[
-         'product_type' => 'required|string',
          'review_id' => 'nullable|integer|exists:product_reviews,id',
          'product_id' => 'required_if:review_id,null|integer|exists:products,id',
-         'variation_id' => 'nullable|integer',
          'limit' => 'nullable|integer'
       ]);
       return $this->valResult($val);
@@ -32,8 +30,6 @@ class GetReviews extends Action{
 
    protected function getProductReviewQuery(){
       $query = ProductReview::where('product_id',$this->request->query('product_id'))
-      ->where('variation_id',$this->request->query('variation_id'))
-      ->where('product_type',$this->request->query('product_type'))
       ->where('status',$this->getResourceActiveId());
       return $query;
    }
@@ -44,12 +40,11 @@ class GetReviews extends Action{
       } else {
          $query = $this->getProductReviewQuery();
          $result = $query->paginate($this->request->query('limit',30));
-         return $this->appendSummary($result);
+         return $this->appendSummary($result,$query);
       }
    }
 
-   protected function appendSummary($data){
-      $query = $this->getProductReviewQuery();
+   protected function appendSummary($data,$query){
       $all_reviews = $query->get();
       $collection = collect([
          'review_average'=>$this->getReviewAverage($all_reviews,"star_rating"),
