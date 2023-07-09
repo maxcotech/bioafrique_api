0bd554d4cb57888cfdbc5526ac30fc4e066865ea
Chisom Maxwell <chiscomax2013@gmail.com>
2023-07-09 02:11:22 +0100

    Add Currency to filters

--- App/Traits/HasProductFilters.php
+++ App/Traits/HasProductFilters.php
@@ -2,22 +2,25 @@
 
 namespace App\Traits;
 
+use App\Http\Resources\CurrencyResource;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\Store;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 trait HasProductFilters
 {
     use HasResourceStatus, HasArrayOperations, HasRateConversion, HasCategory;
-    use HasRoles,HasStore;
+    use HasRoles, HasStore;
 
-    protected function getStoreValidationRule($auth_type,$user){
+    protected function getStoreValidationRule($auth_type, $user)
+    {
         $nullable_rule = "nullable|integer|exists:stores,id";
-        if($auth_type->type == User::auth_type && isset($user)){
+        if ($auth_type->type == User::auth_type && isset($user)) {
             $user_type = $user->user_type;
-            if($this->isStoreOwner($user_type) || $this->isStoreStaff($user_type)){
+            if ($this->isStoreOwner($user_type) || $this->isStoreStaff($user_type)) {
                 return $this->storeIdValidationRule();
             } else {
                 return $nullable_rule;
@@ -36,8 +39,8 @@ trait HasProductFilters
                 ->chunkById(100, function ($products) use ($rating, &$selected_ids) {
                     foreach ($products as $product) {
                         $review_average = $product->reviews()
-                        ->where('status',$this->getResourceActiveId())
-                        ->avg('star_rating');
+                            ->where('status', $this->getResourceActiveId())
+                            ->avg('star_rating');
                         if ($review_average >= $rating) {
                             if (!in_array($product->id, $selected_ids)) {
                                 array_push($selected_ids, $product->id);
@@ -50,23 +53,23 @@ trait HasProductFilters
         return $query;
     }
 
-    protected function filterByProductStatus($query,$auth_type,$user){
-        if($auth_type->type == User::auth_type && isset($user)){
-           $user_type = $user->user_type;
-           if($this->isStoreOwner($user_type) || $this->isStoreStaff($user_type) || $this->isSuperAdmin($user_type)){
-              $status = $this->request->query('status',null);
-              if($status != null){
-                 $query = $query->where('product_status',$status);
-              }
-           } else {
-              $query = $query->where('product_status',$this->getResourceActiveId());
-           }
+    protected function filterByProductStatus($query, $auth_type, $user)
+    {
+        if ($auth_type->type == User::auth_type && isset($user)) {
+            $user_type = $user->user_type;
+            if ($this->isStoreOwner($user_type) || $this->isStoreStaff($user_type) || $this->isSuperAdmin($user_type)) {
+                $status = $this->request->query('status', null);
+                if ($status != null) {
+                    $query = $query->where('product_status', $status);
+                }
+            } else {
+                $query = $query->where('product_status', $this->getResourceActiveId());
+            }
         } else {
-           $query = $query->where('product_status',$this->getResourceActiveId());
+            $query = $query->where('product_status', $this->getResourceActiveId());
         }
         return $query;
-  
-     }
+    }
 
     protected function filterBySearchQuery($query)
     {
@@ -91,7 +94,7 @@ trait HasProductFilters
             if (isset($city_id)) {
                 $store_query = $store_query->where('city_id', $city_id);
             }
-            $store_ids = json_decode(json_encode($store_query->pluck('id')),true);
+            $store_ids = json_decode(json_encode($store_query->pluck('id')), true);
             return $query->whereIn('store_id', $store_ids);
         }
         return $query;
@@ -100,57 +103,63 @@ trait HasProductFilters
     protected function getProductFilterArray($category_id = null, $search_query = null)
     {
         $category_filters = [];
-        $cat_select = ['id','category_title','display_title','category_slug','category_image','category_icon'];
-        $query = Product::select('id','regular_price','sales_price','category_id','brand_id');
-        if(isset($search_query)){
-            $query = $query->where('product_name','LIKE',"%$search_query%");
+        $cat_select = ['id', 'category_title', 'display_title', 'category_slug', 'category_image', 'category_icon'];
+        $query = Product::select('id', 'regular_price', 'sales_price', 'category_id', 'brand_id');
+        if (isset($search_query)) {
+            $query = $query->where('product_name', 'LIKE', "%$search_query%");
         }
-        if(isset($category_id)){
+        if (isset($category_id)) {
             $category = Category::find($category_id);
             $category_filters['main_category'] = $category;
             $category_filters['categories'] = $category->subCategories()
-            ->where('status',$this->getResourceActiveId())->get($cat_select);
-            if(isset($category)){
-                if(Category::where('parent_id',$category->id)->exists()){
+                ->where('status', $this->getResourceActiveId())->get($cat_select);
+            if (isset($category)) {
+                if (Category::where('parent_id', $category->id)->exists()) {
                     $sub_cats = $this->getAllCategoryDescendants($category);
-                    $cat_ids = $this->extractUniqueValueList($sub_cats,'id');
-                    array_push($cat_ids,$category->id);
-                    $query = $query->whereIn('category_id',$cat_ids);
+                    $cat_ids = $this->extractUniqueValueList($sub_cats, 'id');
+                    array_push($cat_ids, $category->id);
+                    $query = $query->whereIn('category_id', $cat_ids);
                 } else {
-                    $query = $query->where('category_id',$category->id);
+                    $query = $query->where('category_id', $category->id);
                 }
             }
         } else {
-            $category_filters['categories'] = Category::where('category_level',Category::MAIN_CATEGORY_LEVEL)
-            ->where('status',$this->getResourceActiveId())->get($cat_select);
+            $category_filters['categories'] = Category::where('category_level', Category::MAIN_CATEGORY_LEVEL)
+                ->where('status', $this->getResourceActiveId())->get($cat_select);
         }
         $products = $query->get();
+        $cookie = $this->getUserByCookie();
+        $user = Auth::user();
+        $currency = $this->getUserCurrency($user, $cookie);
         $category_filters['brands'] = $this->getBrandFilters($products);
         $category_filters['price_range'] = $this->getPriceFilters($products);
+        $category_filters['currency'] = (isset($currency)) ? new CurrencyResource($currency) : null;
         return $category_filters;
     }
 
-    protected function getBrandFilters($products){
-        $brand_ids = $this->extractUniqueValueList($products,'brand_id');
-        return Brand::select('id','brand_name','brand_logo')
-        ->whereIn('id',$brand_ids)->get();
+    protected function getBrandFilters($products)
+    {
+        $brand_ids = $this->extractUniqueValueList($products, 'brand_id');
+        return Brand::select('id', 'brand_name', 'brand_logo')
+            ->whereIn('id', $brand_ids)->get();
     }
 
-    protected function getPriceFilters($products){
+    protected function getPriceFilters($products)
+    {
         $max_price = 0;
         $min_price = 0;
-        foreach($products as $product){
+        foreach ($products as $product) {
             $price = 0;
-            if($product->sales_price != null && $product->sales_price !== 0){
+            if ($product->sales_price != null && $product->sales_price !== 0) {
                 $price = $product->sales_price;
             } else {
                 $price = $product->regular_price;
             }
-            if($price > $max_price) $max_price = $price;
-            if($min_price == 0){
+            if ($price > $max_price) $max_price = $price;
+            if ($min_price == 0) {
                 $min_price = $price;
             } else {
-                if($price < $min_price && $price > 0) $min_price = $price;
+                if ($price < $min_price && $price > 0) $min_price = $price;
             }
         }
         return [
