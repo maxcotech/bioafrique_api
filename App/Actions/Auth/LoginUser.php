eeb01a25fd73956ec5eca45347be08c63760ca2b
Chisom Maxwell <chiscomax2013@gmail.com>
2023-07-16 01:30:18 +0100

    Add sync search history

--- App/Actions/Auth/LoginUser.php
+++ App/Actions/Auth/LoginUser.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Actions\Auth;
+
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Actions\Action;
@@ -9,47 +11,55 @@ use App\Traits\SyncUserPreferences;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class LoginUser extends Action{
-    use SyncUserPreferences;
+class LoginUser extends Action
+{
+   use SyncUserPreferences;
 
-    protected $request;
-    public function __construct(Request $request){
-       $this->request=$request;
-    }
-    protected function validate(){
-       $val = Validator::make($this->request->all(),[
+   protected $request;
+   public function __construct(Request $request)
+   {
+      $this->request = $request;
+   }
+   protected function validate()
+   {
+      $val = Validator::make($this->request->all(), [
          'email' => 'required|email|exists:users,email',
          'password' => 'required|string',
-       ]);
-       return $this->valResult($val);
-    }
-    protected function isCredencialsCorrect($record){
-       if(!isset($record)) return false;
-       if(Hash::check($this->request->password,$record->password)){
-          return true;
-       }else{
-          return false;
-       }
-    }
-    protected function generateToken($record){
+      ]);
+      return $this->valResult($val);
+   }
+   protected function isCredencialsCorrect($record)
+   {
+      if (!isset($record)) return false;
+      if (Hash::check($this->request->password, $record->password)) {
+         return true;
+      } else {
+         return false;
+      }
+   }
+   protected function generateToken($record)
+   {
       $token = $record->createToken('Personal Access Token')->accessToken;
       return $token;
-    }
-    protected function syncUserData($user){
-      if(!isset($user)) return;
+   }
+   protected function syncUserData($user)
+   {
+      if (!isset($user)) return;
       $cookie_pin = $this->request->cookie("basic_access");
-      $cookie = Cookie::where('cookie_value',$cookie_pin)->first();
-      if(!isset($cookie)) return;
-      DB::transaction(function() use($user,$cookie){
-         $this->syncUserCurrency($cookie->id,$user->id);
-         $this->syncUserCountry($cookie->id,$user->id);
-         $this->syncUserCart($cookie->id,$user->id);
-         $this->syncUserRecentlyViewed($cookie->id,$user->id);
-         $this->syncUserWishList($cookie->id,$user->id);
+      $cookie = Cookie::where('cookie_value', $cookie_pin)->first();
+      if (!isset($cookie)) return;
+      DB::transaction(function () use ($user, $cookie) {
+         $this->syncUserCurrency($cookie->id, $user->id);
+         $this->syncUserCountry($cookie->id, $user->id);
+         $this->syncUserCart($cookie->id, $user->id);
+         $this->syncUserRecentlyViewed($cookie->id, $user->id);
+         $this->syncUserWishList($cookie->id, $user->id);
+         $this->syncSearchHistory($cookie->id, $user->id);
       });
-    }
-    protected function getTokenCookie($token){
-       return cookie(
+   }
+   protected function getTokenCookie($token)
+   {
+      return cookie(
          '_token', //name
          $token, //value
          6 * 30 * 24 * 60, //minutes
@@ -59,29 +69,27 @@ class LoginUser extends Action{
          true, //httponly
          false, //raw
          null, //same site
-       );
-    }
-    public function execute(){
-       try{
-          $val = $this->validate();
-          if($val['status'] != "success") return $this->resp($val);
-          $record = User::where('email',$this->request->email)->first();
-          if($this->isCredencialsCorrect($record)){
-             $token = $this->generateToken($record);
-             $this->syncUserData($record);
-             $cookie = $this->getTokenCookie($token);
-             return $this->successWithData([
-               'token'=>$token,
-               'user_type'=>$record->user_type,
-             ])->withCookie($cookie);
-          }else{
-             return $this->validationError('Incorrect email or password entered');
-          }
-       }
-       catch(\Exception $e){
-          return $this->internalError($e->getMessage());
-       }
-    }
-
+      );
+   }
+   public function execute()
+   {
+      try {
+         $val = $this->validate();
+         if ($val['status'] != "success") return $this->resp($val);
+         $record = User::where('email', $this->request->email)->first();
+         if ($this->isCredencialsCorrect($record)) {
+            $token = $this->generateToken($record);
+            $this->syncUserData($record);
+            $cookie = $this->getTokenCookie($token);
+            return $this->successWithData([
+               'token' => $token,
+               'user_type' => $record->user_type,
+            ])->withCookie($cookie);
+         } else {
+            return $this->validationError('Incorrect email or password entered');
+         }
+      } catch (\Exception $e) {
+         return $this->internalError($e->getMessage());
+      }
+   }
 }
-    
--- App/Actions/ShoppingCart/GetShoppingCart.php
+++ App/Actions/ShoppingCart/GetShoppingCart.php
@@ -1,45 +1,57 @@
 <?php
+
 namespace App\Actions\ShoppingCart;
+
 use Illuminate\Http\Request;
 use App\Actions\Action;
+use App\Http\Resources\CurrencyResource;
 use App\Models\ShoppingCartItem;
 use App\Traits\HasAuthStatus;
 use App\Traits\HasProduct;
+use App\Traits\HasRateConversion;
 use App\Traits\HasShoppingCartItem;
+use Illuminate\Support\Facades\Auth;
 
-class GetShoppingCart extends Action{
-   use HasAuthStatus,HasShoppingCartItem,HasProduct;
+class GetShoppingCart extends Action
+{
+   use HasAuthStatus, HasShoppingCartItem, HasProduct, HasRateConversion;
    protected $request;
-   public function __construct(Request $request){
-      $this->request=$request;
+   public function __construct(Request $request)
+   {
+      $this->request = $request;
    }
 
-   protected function onGetShoppingCart($auth_type){
+   protected function onGetShoppingCart($auth_type)
+   {
       return ShoppingCartItem::with([
-         'variation:id,variation_image,variation_name',
-         'product:id,product_name,product_image,regular_price,sales_price',
+         'variation:id,variation_image,variation_name,regular_price,sales_price,amount_in_stock',
+         'product:id,product_name,product_image,regular_price,sales_price,amount_in_stock',
          'store:id,store_name,store_slug'
       ])
-      ->where('user_id',$auth_type->id)
-      ->where('user_type',$auth_type->type)
-      ->paginate(15,['id','item_id','variant_id','item_type','quantity','store_id']);
+         ->where('user_id', $auth_type->id)
+         ->where('user_type', $auth_type->type)
+         ->paginate(15, ['id', 'item_id', 'variant_id', 'item_type', 'quantity', 'store_id']);
    }
 
 
-   public function execute(){
-      try{
+   public function execute()
+   {
+      try {
          $auth_type_obj = $this->getUserAuthTypeObject();
-         if(!isset($auth_type_obj)) throw new \Exception('An Error occurred, please ensure your browser enables usage of cookies.');
+         if (!isset($auth_type_obj)) throw new \Exception('An Error occurred, please ensure your browser enables usage of cookies.');
          $data = $this->onGetShoppingCart($auth_type_obj);
-         $data = $this->appendWishListStatus($data,$auth_type_obj,"item_id");
-         $collect = collect(['cart_count'=>$this->getTotalCartCount($auth_type_obj)]);
+         $data = $this->appendWishListStatus($data, $auth_type_obj, "item_id");
+         $cookie = $this->getUserByCookie();
+         $user = Auth::user();
+         $currency = $this->getUserCurrency($user, $cookie);
+         $collect = collect([
+            'currency' => isset($currency) ? new CurrencyResource($currency) : null,
+            'cart_count' => $this->getTotalCartCount($auth_type_obj)
+         ]);
          $data = $collect->merge($data);
          return $this->successWithData($data);
-      }
-      catch(\Exception $e){
+      } catch (\Exception $e) {
          return $this->internalError($e->getMessage());
       }
    }
-
 }
-   
