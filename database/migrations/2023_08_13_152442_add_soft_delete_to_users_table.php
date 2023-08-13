fd27ba7155600014937e622a596863f1a0fa0531
Chisom Maxwell <chiscomax2013@gmail.com>
2023-08-13 12:49:55 +0100

    fix soft delete migration

--- database/migrations/2023_08_13_152442_add_soft_delete_to_users_table.php
+++ database/migrations/2023_08_13_152442_add_soft_delete_to_users_table.php
@@ -13,7 +13,7 @@ class AddSoftDeleteToUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->softDeletes();
         });
     }
